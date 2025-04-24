aceacda3cb1b5ead8588f14b58de1667cc27902d
Tobby Willson <adepitantobi@gmail.com>
2025-04-24 23:58:19 +0100

    Newer changes

--- src/Components/Login-signup/LoginPage.jsx
+++ src/Components/Login-signup/LoginPage.jsx
@@ -1,4 +1,6 @@
-import userIcon from "../Assets/user-icon.png";
+import { useState } from "react";
+// import { Link } from "react-router-dom";
+
 import emailIcon from "../Assets/email-icon.png";
 import passwordIcon from "../Assets/password-icon.png";
 import eyeIcon from "../Assets/eye-icon.png";
@@ -6,8 +8,6 @@ import eyeIcon from "../Assets/eye-icon.png";
 import stethoscope from "../Assets/stethoscope.png";
 
 import "./Login.css";
-
-import { useState } from "react";
 
 const LoginPage = () => {
   const [showPass, setShowPass] = useState(true);
@@ -21,99 +21,53 @@ const LoginPage = () => {
     setShowPass(!showPass);
   };
 
-  const [action, setAction] = useState("Sign up");
-
   return (
-    <div>
-      <div className='container'>
-        <div className='container-bg'>
-          <img src={stethoscope} alt='' />
+    <div className='container'>
+      <div className='container-bg'>
+        <img src={stethoscope} alt='' />
+      </div>
+
+      <div className='header'>
+        <div className='header-text'>Log in</div>
+        <div className='underline'></div>
+      </div>
+
+      <div className='inputs'>
+        <div className='input'>
+          <img src={emailIcon} alt='' />
+          <input type='email' placeholder='Email Address' required />
         </div>
 
-        <div className='header'>
-          <div className='header-text'>{action}</div>
-          <div className='underline'></div>
+        <div className='input'>
+          <img src={passwordIcon} alt='' />
+          <input type={showPass ? "password" : "text"} onChange={insertPass} name='login-pass' placeholder='Password' />
+          {insert ? <img src={eyeIcon} alt='' className='passEye' onClick={toggleVisibility} /> : ""}
         </div>
+      </div>
 
-        <div className='inputs'>
-          {action === "Login" ? (
-            <></>
-          ) : (
-            <div className='input'>
-              <img src={userIcon} alt='' />
-              <input type='text' placeholder='Username' />
-            </div>
-          )}
-          <div className='input'>
-            <img src={emailIcon} alt='' />
-            <input type='email' placeholder='Email Address' required />
-          </div>
-          {action === "Sign up" ? (
-            <div className='input'>
-              <img src={passwordIcon} alt='' />
-              <input type={showPass ? "password" : "text"} name='signup-pass1' placeholder='Password' />
-            </div>
-          ) : (
-            <div className='input'>
-              <img src={passwordIcon} alt='' />
-              <input type={showPass ? "password" : "text"} onChange={insertPass} name='login-pass' placeholder='Password' />
-              {insert ? <img src={eyeIcon} alt='' className='passEye' onClick={toggleVisibility} /> : ""}
-            </div>
-          )}
-          {action === "Login" ? (
-            <></>
-          ) : (
-            <div className='input'>
-              <img src={passwordIcon} alt='' />
-              <input type={showPass ? "password" : "text"} onChange={insertPass} name='signup-pass2' placeholder='Confirm Password' />
-              {insert ? <img src={eyeIcon} alt='' className='passEye' onClick={toggleVisibility} /> : ""}
-            </div>
-          )}
-        </div>
-
-        {action === "Sign up" ? (
-          <></>
+      <div className='remember'>
+        {insert ? (
+          <>
+            <input type='checkbox' name='remember' />
+            <p>Remember this device!</p>{" "}
+          </>
         ) : (
-          <div className='remember'>
-            {insert ? (
-              <>
-                <input type='checkbox' name='remember' />
-                <p>Remember this device!</p>{" "}
-              </>
-            ) : (
-              ""
-            )}
-          </div>
+          ""
         )}
+      </div>
 
-        <div className='submitContainer'>
-          {insert ? <div className='button'>{action}</div> : ""}
-          {action === "Login" ? (
-            <div className='login'>
-              <div className='forgot'>
-                Forgot password? <span>Click here</span>
-              </div>
-              Do not have an account yet?{" "}
-              <span
-                onClick={() => {
-                  setAction("Sign up");
-                }}
-              >
-                Sign up
-              </span>
-            </div>
-          ) : (
-            <div className='login'>
-              Already have an account?{" "}
-              <span
-                onClick={() => {
-                  setAction("Login");
-                }}
-              >
-                Login
-              </span>
-            </div>
-          )}
+      <div className='submitContainer'>
+        {insert ? <div className='button'>Log in</div> : ""}
+        <div className='account-checker'>
+          <div className='forgot'>
+            <p>Forgot password?</p>
+            <a href='/forgot-password'>Click Here</a>
+          </div>
+
+          <div className='signup'>
+            <p>Do not have an account yet?</p>
+            <a href='/signup'>Sign up</a>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Login-signup/SignupPage.jsx
+++ src/Components/Login-signup/SignupPage.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import userIcon from "../Assets/user-icon.png";
+import emailIcon from "../Assets/email-icon.png";
+import passwordIcon from "../Assets/password-icon.png";
+import eyeIcon from "../Assets/eye-icon.png";
+
+import stethoscope from "../Assets/stethoscope.png";
+
+import "./Login.css";
+
+import { useState } from "react";
 
 const SignupPage = () => {
-  return <div></div>;
+  const [showPass, setShowPass] = useState(true);
+  const [insert, setInsert] = useState("");
+
+  const insertPass = (e) => {
+    setInsert(e.target.value);
+  };
+
+  const toggleVisibility = () => {
+    setShowPass(!showPass);
+  };
+
+  const [action, setAction] = useState("Sign up");
+
+  return (
+    <div>
+      <div className='container'>
+        <div className='container-bg'>
+          <img src={stethoscope} alt='' />
+        </div>
+
+        <div className='header'>
+          <div className='header-text'>Sign up</div>
+          <div className='underline'></div>
+        </div>
+
+        <div className='inputs'>
+          <div className='input'>
+            <img src={userIcon} alt='' />
+            <input type='text' placeholder='Username' />
+          </div>
+
+          <div className='input'>
+            <img src={emailIcon} alt='' />
+            <input type='email' placeholder='Email Address' required />
+          </div>
+
+          <div className='input'>
+            <img src={passwordIcon} alt='' />
+            <input type={showPass ? "password" : "text"} name='signup-pass1' placeholder='Password' />
+          </div>
+
+          <div className='input'>
+            <img src={passwordIcon} alt='' />
+            <input type={showPass ? "password" : "text"} onChange={insertPass} name='signup-pass2' placeholder='Confirm Password' />
+            {insert ? <img src={eyeIcon} alt='' className='passEye' onClick={toggleVisibility} /> : ""}
+          </div>
+        </div>
+
+        <div className='submitContainer'>
+          {insert ? <div className='button'>{action}</div> : ""}
+          <div className='account-checker'>
+            <div className='login'>
+              <p>Already have an account?</p>
+              <a href='/login'>Login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
