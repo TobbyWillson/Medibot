69ac5f0f042f321734471018bc93ea05c3961340
Tobby Willson <adepitantobi@gmail.com>
2025-04-29 14:10:40 +0100

    Update

--- src/Components/Login-signup/HomePage.jsx
+++ src/Components/Login-signup/HomePage.jsx
@@ -2,6 +2,7 @@ import "./Login.css";
 import { Link } from "react-router-dom";
 import android from "../Assets/android-robot.png";
 import { FaStethoscope } from "react-icons/fa";
+import Footer from "./Footer";
 
 const HomePage = () => {
   return (
@@ -31,6 +32,22 @@ const HomePage = () => {
           </div>
         </div>
         <div className='underline'></div>
+
+        <div className='screen-content'>
+          <div className='robot-img'>
+            <img src={android} alt='' />
+          </div>
+
+          <div className='others'>
+            <h1>Meet Your New AI Companion</h1>
+            <p>A chatbot that specializes in analyzing, diagonising and giving medical attentions to users based on inputs, get in touch today!</p>
+            <p className='version'>v1.0</p>
+            <p className='welcome-message'>It is a pleasure to meet you! How can I assist you today?</p>
+            <Link to='/login' className='get-started'>
+              Get Started
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
