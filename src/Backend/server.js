364d51b8d6f7c49e1742e2509a71066a999945c6
Tobby Willson <adepitantobi@gmail.com>
2025-08-27 22:05:37 +0100

    New changes

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -39,14 +39,6 @@ mongoose
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-app.get(`${process.env.REACT_APP_API_URL}/test-mongo`, async (req, res) => {
-  try {
-    const dbList = await mongoose.connection.db.admin().listDatabases();
-    res.json({ databases: dbList.databases });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
 app.get("/test-mongo", async (req, res) => {
   try {
     const dbList = await mongoose.connection.db.admin().listDatabases();
